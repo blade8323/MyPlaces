234e5e2ff77ed87371faf22915c706fb4aee3f2e
blade8323 <blade8323@mail.ru>
2021-10-16 22:13:39 +0500

    customize cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Владислав Соколов on 16.10.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+   
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
